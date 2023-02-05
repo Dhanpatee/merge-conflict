11bd12cf1127b1f9d57d980d1c93c999cef1d7dc
dhanapti <dhanapti.dhn@gmil.com>
2023-02-05 09:33:15 +0530

    this commit  for feature2

--- main.c
+++ main.c
@@ -1,15 +1,22 @@
 pseudo code
 {
-Initial Function()
-{
-  //Sample Code
-}
+    Initial Function()
+  {
+    //Sample Code
+  }
 
-Initial Security()
-{
- //Sample Code
-}
+  Initial Security()
+  {
+    //Sample Code
+  }
+  Feature1 Security()
+  {
+    //security code from feature1
+  }
 
-
+ Feature2 Security()
+ {
+   // feature2 security patch
+ }
 
 }
